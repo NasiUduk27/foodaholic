9cee0d36fcf021e584e4c1622031f5da16aaee02
= <dhoriffitodiansyah27@gmail.com>
2023-05-29 17:14:50 +0700

    fix faq and about us

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -11,7 +11,7 @@
             <div class="col-12 col-sm-8 col-lg-6">
                 <div class="section_heading text-center wow fadeInUp" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
                     <h3><span>Frequently </span> Asked Questions</h3>
-                    <p>Appland is completely creative, lightweight, clean &amp; super responsive app landing page.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, debitis veniam! Officia, magni, officiis deleniti quas praesentium explicabo at illum unde et ipsum aut molestias veniam modi quod ut ipsam?</p>
                     <div class="line"></div>
                 </div>
             </div>
@@ -22,34 +22,34 @@
                 <div class="accordion faq-accordian" id="faqAccordion">
                     <div class="card border-0 wow fadeInUp" data-wow-delay="0.2s" style="visibility: visible; animation-delay: 0.2s; animation-name: fadeInUp;">
                         <div class="card-header" id="headingOne">
-                            <h6 class="mb-0 collapsed" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">How can I install this app?<span class="lni-chevron-up"></span></h6>
+                            <h6 class="mb-0 collapsed" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Lorem ipsum dolor sit amet<span class="lni-chevron-up"></span></h6>
                         </div>
                         <div class="collapse" id="collapseOne" aria-labelledby="headingOne" data-parent="#faqAccordion">
                             <div class="card-body">
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quidem facere deserunt sint animi sapiente vitae suscipit.</p>
-                                <p>Appland is completely creative, lightweight, clean &amp; super responsive app landing page.</p>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, rerum quo nulla ratione impedit placeat adipisci id cupiditate earum dolorem necessitatibus dolorum eveniet harum magnam modi facere quasi in voluptates!</p>
                             </div>
                         </div>
                     </div>
                     <div class="card border-0 wow fadeInUp" data-wow-delay="0.3s" style="visibility: visible; animation-delay: 0.3s; animation-name: fadeInUp;">
                         <div class="card-header" id="headingTwo">
-                            <h6 class="mb-0 collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">The apps isn't installing?<span class="lni-chevron-up"></span></h6>
+                            <h6 class="mb-0 collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">Lorem ipsum dolor sit amet<span class="lni-chevron-up"></span></h6>
                         </div>
                         <div class="collapse" id="collapseTwo" aria-labelledby="headingTwo" data-parent="#faqAccordion">
                             <div class="card-body">
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quidem facere deserunt sint animi sapiente vitae suscipit.</p>
-                                <p>Appland is completely creative, lightweight, clean &amp; super responsive app landing page.</p>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In consequuntur dolor, similique dignissimos nihil animi labore quas cum. Saepe quisquam quidem rem perferendis quos eum sequi tempore ullam iste ex.</p>
                             </div>
                         </div>
                     </div>
                     <div class="card border-0 wow fadeInUp" data-wow-delay="0.4s" style="visibility: visible; animation-delay: 0.4s; animation-name: fadeInUp;">
                         <div class="card-header" id="headingThree">
-                            <h6 class="mb-0 collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">Contact form isn't working?<span class="lni-chevron-up"></span></h6>
+                            <h6 class="mb-0 collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">Lorem ipsum dolor sit amet<span class="lni-chevron-up"></span></h6>
                         </div>
                         <div class="collapse" id="collapseThree" aria-labelledby="headingThree" data-parent="#faqAccordion">
                             <div class="card-body">
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quidem facere deserunt sint animi sapiente vitae suscipit.</p>
-                                <p>Appland is completely creative, lightweight, clean &amp; super responsive app landing page.</p>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In consequuntur dolor, similique dignissimos nihil animi labore quas cum. Saepe quisquam quidem rem perferendis quos eum sequi tempore ullam iste ex.</p>
                             </div>
                         </div>
                     </div>
