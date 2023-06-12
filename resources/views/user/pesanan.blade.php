fd824f4089994e537666a7cd505d9e203c2d632a
= <dhoriffitodiansyah27@gmail.com>
2023-06-12 11:21:00 +0700

    pesanan salah

--- resources/views/user/pesanan.blade.php
+++ resources/views/user/pesanan.blade.php
@@ -5,13 +5,30 @@
             @foreach($pesanan as $p)
 
             <div class="container">
-                <h4>{{ $p->nama_mitra }}</h4>
-                <img src="{{ asset('images/'. $p->foto_produk) }}" alt="Image">
-                <p>{{ $p->nama_produk }}</p>
-                <p>{{ $p->harga }}</p>
-                <p>{{ $p->qty }}</p>
-
-            </div>
+                <table class="table table-bordered">
+                    <thead>
+                      <tr>
+                        <th>Nama Produk</th>
+                        <th>Harga</th>
+                        <th>Qty</th>
+                        <th>Gambar</th>
+                        <th>Nama Mitra</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>{{ $p->nama_produk }}</td>
+                        <td>{{ $p->harga }}</td>
+                        <td>{{ $p->qty }}</td>
+                        <td><img src="{{ asset('images/fast-food.jpg'. $p->foto_produk) }}" style="max-width: 100px;" alt="Image"></td>
+                        <td>{{ $p->nama_mitra }}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  
+                  
+              </div>
+              
 
             @endforeach
 @endsection
